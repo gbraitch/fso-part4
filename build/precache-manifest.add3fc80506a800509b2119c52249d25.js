2df10b8b054070e012afea0d56fbfe1f805e6cbb
Gursewak Braitch <g.braitch@hotmail.com>
2020-08-24 20:09:46 -0700

    Ran new frontend build

---
+++ build/precache-manifest.add3fc80506a800509b2119c52249d25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f38df1a8ecfd18a89dbce148b3b5f30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1f13bf4974ce979c951",
+    "url": "/static/css/2.c31a3e37.chunk.css"
+  },
+  {
+    "revision": "f7c42df2aebab0ce21da",
+    "url": "/static/css/main.3c5b2d1d.chunk.css"
+  },
+  {
+    "revision": "f1f13bf4974ce979c951",
+    "url": "/static/js/2.ffdb9f42.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.ffdb9f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c42df2aebab0ce21da",
+    "url": "/static/js/main.303f6c3e.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
